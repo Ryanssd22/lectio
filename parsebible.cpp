0a5e11d425c8c76912bf3fe5e219ae50d3424e5c
Benedict Simeon <benedictsimeon@my.unt.edu>
2025-05-01 11:10:38 -0500

    Fix some parsing

--- parsebible.cpp
+++ parsebible.cpp
@@ -3,11 +3,12 @@
 #include <fstream>
 #include <sstream>
 #include <cctype>
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  if (argc != 5) {
-    cout << "Usage: " << argv[0] << " [FILE] [BOOK] [CHAPTER] [VERSE]" << endl;
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " [FILE]";
     return 1;
   }
 
@@ -17,14 +18,12 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  string inputBook = argv[2];
-  int inputChapter = stoi(argv[3]);
-  int inputVerse = stoi(argv[4]);
   string line, currentBook, version, verabbr;
 
   getline(bible,verabbr);
   getline(bible,version);
   cout << "Bible version: " << version << endl;
+  ofstream bibleOutput(verabbr + ".txt");
   while (getline(bible, line)) {
     //Reads book
     string newBook;
@@ -35,23 +34,22 @@ int main(int argc, char* argv[]) {
       verse >> secondTitle;
       newBook += secondTitle;
     }
-    //if (currentBook != newBook) cout << "Book: " << newBook << '\n';
+    transform(newBook.begin(), newBook.end(), newBook.begin(), ::toupper);
+    if (currentBook != newBook) {
+      cout << newBook << endl;
+    }
     currentBook = newBook;
 
-    if (currentBook == inputBook) {
-      //Reads chapter:verse
-      int chapter, verseNumber;
-      char colon;
-      verse >> chapter >> colon >> verseNumber;
-      //cout << chapter << ":" << verseNumber << '\t';
+    //Reads chapter:verse
+    int chapter, verseNumber;
+    char colon;
+    verse >> chapter >> colon >> verseNumber;
+    //cout << chapter << ":" << verseNumber << " ";
 
-      if (chapter == inputChapter && verseNumber == inputVerse) {
-        //Reads verse
-        string fullVerse;
-        getline(verse, fullVerse);
-        cout << fullVerse << endl;
-      }
-    }
+    //Reads verse
+    string fullVerse;
+    getline(verse, fullVerse);
+    //cout << fullVerse << endl;
   }
   return 0;
 }
