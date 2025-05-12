87ea0acead713c19afca949910fc9d6b81aa9477
Ryanssd22 <carbox7@gmail.com>
2025-05-12 14:58:33 -0500

    Update readLectionary.py

--- liturgicalCalendar/readLectionary.py
+++ liturgicalCalendar/readLectionary.py
@@ -3,6 +3,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def printTableData(key):
+    print(f"{row:75} | {sortedTableData[row]}")
+
+
 weekdayPage = requests.get(
     "https://catholic-resources.org/Lectionary/Index-Weekdays.htm"
 )
@@ -15,15 +20,27 @@ for i, table in enumerate(tables):
     for row in rows:
         cols = row.find_all("td")
         cols = [col.text.strip() for col in cols]
+
+        # cols[0] = Reading
+        # cols[1] = Day/Feast
+        # cols[2] = Year
+        # cols[3] = Lec#
+
         date = cols[1]
+        date = date.replace("Ord. Time", "OT")
+        date = date.replace("Lent", "LE")
+        date = date.replace("Advent", "AD")
+        date = date.replace("Easter", "EA")
         if not date in [".", "Day", "Day or Feast"]:
+            cols.pop(3)
             cols.pop(1)
-            cols.pop(2)
             if not date in tableData:
                 tableData[date] = [cols]
             else:
                 tableData[date].append(cols)
 
+sortedTableData = dict(sorted(tableData.items()))
 
 for row in tableData:
-    print(f"{row:75} | {tableData[row]}")
+    if "OT" in row:
+        printTableData(row)
